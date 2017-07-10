58906c8f974d07af31db9dc8d7dc9ccdb04b8a6b
Joe <jnorby@andrew.cmu.edu>
2017-07-10 15:58:32 -0400

    Add cmake functionality, build dir

---
+++ osgMinitaur/CMakeFiles/osgMinitaur.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joe/dart_minitaur/osgMinitaur/Bound.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/Bound.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/Controller.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/Controller.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/Dig.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/Dig.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/Flip.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/Flip.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/HAL.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/HAL.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/Interpolator.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/Interpolator.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/MinitaurEventHandler.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/MinitaurEventHandler.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/MinitaurLeg.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/MinitaurLeg.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/MinitaurWidget.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/MinitaurWidget.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/MinitaurWorldNode.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/MinitaurWorldNode.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/Motor.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/Motor.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/RSUJump.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/RSUJump.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/Remote.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/Remote.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/State.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/State.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/StateMachine.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/StateMachine.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/TerminalCondition.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/TerminalCondition.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/WMath.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/WMath.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/Walk.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/Walk.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/main.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/main.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/minitaurDARTInterface.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/minitaurDARTInterface.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/pushWalk.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/pushWalk.cpp.o"
+  "/home/joe/dart_minitaur/osgMinitaur/traj.cpp" "/home/joe/dart_minitaur/osgMinitaur/CMakeFiles/osgMinitaur.dir/traj.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
